8d8b487c63cc248b62f45fb805126f9cb5eddbae
Chrysthian Moizes <chrysthianmoizes@gmail.com>
2018-02-20 21:55:29 -0300

    Divisao de conteudo em arquivos HTML

--- web/js/util.js
+++ web/js/util.js
@@ -3,3 +3,27 @@ $("#carrosel").ready(function (){
         $("#carrosel").load("util/carrossel.html");
     });
 });
+
+$("#cabecalho").ready(function (){
+    $(function (){
+        $("#cabecalho").load("util/cabecalho.html");
+    });
+});
+
+$("#rodape").ready(function (){
+    $(function (){
+        $("#rodape").load("util/rodape.html");
+    });
+});
+
+$("#filmes").ready(function (){
+    $(function (){
+        $("#filmes").load("util/filmes.html");
+    });
+});
+
+$("#paginacao").ready(function (){
+    $(function (){
+        $("#paginacao").load("util/paginacao.html");
+    });
+});
