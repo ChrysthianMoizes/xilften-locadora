f427ae2a2833f97992ba2c8c0f7a5f7bcea4487a
Davy <davy.g.c.lima@gmail.com>
2018-03-27 09:03:32 -0300

    remoção de comentários das classes (¬.¬)

--- src/java/model/domain/Item.java
+++ src/java/model/domain/Item.java
@@ -1,17 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package model.domain;
 
 import java.util.Collection;
 import java.util.Date;
 
-/**
- *
- * @author reida
- */
 public class Item {
     private int id;
     private int numSerie;
