2f59672cb7c20da73592c3f84e52f3e2c0b7e1b7
Chrysthian Moizes <2015122760084@cefetes.br>
2018-04-03 09:05:13 -0300

    Classe genérica de persistencia

--- src/java/controler/CtrlCadastrarAtor.java
+++ src/java/controler/CtrlCadastrarAtor.java
@@ -9,25 +9,24 @@ import javax.servlet.http.HttpServletResponse;
 import model.application.AplCadastrarAtor;
 
 
-public class ctrCadastrarAtor extends HttpServlet {
+public class CtrlCadastrarAtor extends HttpServlet {
+    
+    private AplCadastrarAtor aplCadastrarAtor;
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+    public CtrlCadastrarAtor() {
+        aplCadastrarAtor = new AplCadastrarAtor();
+    }
+    
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         String operacao = request.getParameter("operacao");
         if(operacao.equals("incluirAtor")){
             String nomeAtor = request.getParameter("nome");
-            int op = AplCadastrarAtor.incluirAtor(nomeAtor);
+            int op = aplCadastrarAtor.incluirAtor(nomeAtor);
+            
             switch(op){
+                
                 case 0:
                     
                 case 1:
@@ -43,42 +42,21 @@ public class ctrCadastrarAtor extends HttpServlet {
         }
     }
 
-        /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
     }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
+   
     @Override
     public String getServletInfo() {
         return "Short description";
-    }// </editor-fold>
+    }
 
 }
