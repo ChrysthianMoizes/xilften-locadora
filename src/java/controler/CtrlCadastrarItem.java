6b0e7460fd2c5b384b8f3451f242d33ca67301a2
Chrysthian Moizes <2015122760084@cefetes.br>
2018-05-10 09:26:21 -0300

    Limpeza de Classes

--- src/java/controler/CtrlCadastrarItem.java
+++ src/java/controler/CtrlCadastrarItem.java
@@ -17,16 +17,7 @@ public class CtrlCadastrarItem extends HttpServlet {
     public CtrlCadastrarItem(){
         aplCadastrarItem = new AplCadastrarItem();
     }
-    
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
@@ -48,48 +39,26 @@ public class CtrlCadastrarItem extends HttpServlet {
             out.print("Erro "+e.getMessage());
         }
     }
-    
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
     }
-
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+    
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
     }
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
     @Override
     public String getServletInfo() {
         return "Short description";
     }// </editor-fold>
 
     private void cadastrar(HttpServletRequest request, HttpServletResponse response) throws IOException, Exception {
+        
         String numSerie = request.getParameter("numSerie");
         String idTitulo = request.getParameter("titulo");//tem que ser diferente de 0
         String dtAquisicao = request.getParameter("dtAquisicao");
