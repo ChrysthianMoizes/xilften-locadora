bde475fe4fd3331b08215e4058ea9f4e6cb0976a
Davy Lima <davy.g.c.lima@gmail.com>
2018-06-06 20:15:23 -0300

    inicio do filtro de login

---
+++ src/java/model/application/AplUsuario.java
@@ -0,0 +1,9 @@
+package model.application;
+
+public class AplUsuario {
+
+    public static boolean logar(String usuario, String senha) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
